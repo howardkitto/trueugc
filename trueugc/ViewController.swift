3b6f956d053700234297602be1d83f4d10a2d76d
Howard Kitto <kitto.howard@gmail.com>
2017-12-08 16:57:27 +0000

    installed lflivekit

--- trueugc/ViewController.swift
+++ trueugc/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+     @IBOutlet weak var infoLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func startButtonPressed(){
+        infoLabel.text="connecting stream"
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

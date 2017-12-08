187eaed992d20f3d7b543083b15f458731e6e416
Howard Kitto <kitto.howard@gmail.com>
2017-12-08 14:35:38 +0000

    Initial Commit

---
+++ trueugc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  trueugc
+//
+//  Created by Howard Kitto on 08/12/2017.
+//  Copyright © 2017 Howard Kitto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

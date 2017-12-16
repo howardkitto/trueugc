2d35c5fea09d25e8febb17727c1eaf181117180e
Howard Kitto <kitto.howard@gmail.com>
2017-12-16 07:55:19 +0000

    got video player working

--- trueugc/VideoPlayViewController.swift
+++ trueugc/VideoPlayViewController.swift
@@ -7,26 +7,49 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
 
 class VideoPlayViewController: UIViewController {
     
     @IBOutlet weak var VideoTitle: UILabel!
     
-    @IBOutlet weak var streamUrl: UILabel!
-    
-    
-    @IBOutlet weak var VideoPLayer: UIView!
+    @IBOutlet weak var streamUrlLabel: UILabel!
     
     var video:Video?
+    
+    var streamUrl :String?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        streamUrl = video?.liveFeeds![0].m3u8Url
+        
         VideoTitle.text = video?.title
         
-//        streamUrl.text = video?.liveFeeds![0].m3u8Url
+        streamUrlLabel.text = streamUrl
 
+    }
+    
+    @IBAction func playVideo(_ sender: Any) {
+        
+        guard let url = URL(string: streamUrl!) else {
+            return
+        }
+        
+        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+        let player = AVPlayer(url: url)
+       
+        // Create a new AVPlayerViewController and pass it a reference to the player.
+        let controller = AVPlayerViewController()
+        controller.player = player
+        
+        // Modally present the player and call the player's play() method when complete.
+        present(controller, animated: true) {
+            player.play()
+        }
+        
     }
 
     
